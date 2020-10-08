2f418a03ff8680af97b4f00639f7bafb6674776a
vagrant <skondapalli02@gmail.com>
2020-10-08 22:17:12 +0000

    Pointer test

--- exercise_3_2.cpp
+++ exercise_3_2.cpp
@@ -6,14 +6,77 @@ LinkedList::LinkedList() {
 }
 
 void LinkedList::add_to_front(std::string name) {
+
+	if (head == nullptr) {
+
+		head = new Node();
+		head.name = name;
+
+	} else {
+
+		Node temp = head;
+		head = new Node();
+		head.name = name;
+		head.next = temp;
+
+	}
+
     return;
 }
 
 void LinkedList::add_to_back(std::string name){
+
+	if (head == nullptr) {
+
+		head = new Node();
+		head.name = name;
+
+	} else {
+
+		Node *temp = &head;
+
+		while (temp.next != nullptr) {
+
+			temp = &temp.next;
+
+		}
+
+		Node insert = new Node();
+		insert.name = name;
+
+		*temp.next = insert;
+
+	}
+
+
     return;
 }
 
 void LinkedList::add_at_index(std::string name, int index) {
+
+	Node *temp = &head;
+
+	for (int i = 0; i < index; i++) {
+
+		temp = &temp.next;
+
+	}
+
+	Node insert = new Node();
+	insert.name = name;
+
+	if(*temp == nullptr) {
+
+		temp = insert;
+
+	} else {
+
+		insert.next = *temp.next;
+		*temp = insert;
+
+	}
+
+
     return;
 }
 
